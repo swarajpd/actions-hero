ec7a535b379b84b6f65d05fd766519059d4759d1
Swaraj Dash <swaraj.dash@conology.net>
2024-02-23 11:57:47 +0530

    first commit

---
+++ src/main/java/com/example/actionshero/ActionsHeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.actionshero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/actionshero/ActionsHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionshero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
